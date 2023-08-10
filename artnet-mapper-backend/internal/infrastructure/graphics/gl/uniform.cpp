389d03d4d82e06919eab5a2903bdbd4f375d2359
broose-goose <bruce@polis.tv>
2023-08-09 23:22:38 -0500

    Okay, decent uniform abstraction; create a bunch of shared ptrs, pass them to a vector; on render loop through and set them!

---
+++ artnet-mapper-backend/internal/infrastructure/graphics/gl/uniform.cpp
@@ -0,0 +1,61 @@
+//
+// Created by broosegoose on 8/9/23.
+//
+
+#include "uniform.hpp"
+
+#include <utility>
+
+namespace infrastructure::graphics {
+
+    Uniform::Uniform(std::string name): _name(std::move(name)) {}
+
+    void Uniform::Setup(const GLuint &shaderProgram) {
+        _location = glGetUniformLocation(shaderProgram, _name.c_str());
+        if (_location == -1) {
+            throw std::runtime_error("Uniform::Setup not found in shader: " + _name);
+        }
+    }
+
+    void Uniform::Teardown() {
+        _location = -1;
+    }
+
+    FloatUniformPtr FloatUniform::Create(const std::string &name, const float &default_value) {
+        auto uniform = std::make_shared<FloatUniform>(name, default_value);
+        return std::move(uniform);
+    }
+
+    FloatUniform::FloatUniform(const std::string &name, const float &default_value):
+        Uniform(name),
+        _value(default_value)
+    {}
+
+    void FloatUniform::SetValue(const float &value) {
+        _value = value;
+    }
+
+    void FloatUniform::Set() const {
+        glUniform1f(_location, _value);
+    }
+
+    IntUniformPtr IntUniform::Create(const std::string &name, const int &default_value) {
+        auto uniform = std::make_shared<IntUniform>(name, default_value);
+        return std::move(uniform);
+    }
+
+    IntUniform::IntUniform(const std::string &name, const int &default_value):
+        Uniform(name),
+        _value(default_value)
+    {}
+
+    void IntUniform::SetValue(const int &value) {
+        _value = value;
+    }
+
+    void IntUniform::Set() const {
+        glUniform1i(_location, _value);
+    }
+
+
+}
---
+++ artnet-mapper-backend/internal/infrastructure/graphics/gl/uniform.hpp
@@ -0,0 +1,76 @@
+//
+// Created by broosegoose on 8/9/23.
+//
+
+#ifndef INFRASTRUCTURE_GRAPHICS_UNIFORM_HPP
+#define INFRASTRUCTURE_GRAPHICS_UNIFORM_HPP
+
+#include "../common.hpp"
+
+namespace infrastructure::graphics {
+
+    class Uniform;
+    typedef std::shared_ptr<Uniform> UniformPtr;
+
+    class Uniform {
+    public:
+        explicit Uniform (std::string name);
+        // no copy assignment, no empty assignment
+        Uniform() = delete;
+        Uniform (const Uniform&) = delete;
+        Uniform& operator= (const Uniform&) = delete;
+    protected:
+        friend class infrastructure::Graphics;
+        virtual void Set() const = 0;
+        void Teardown();
+    protected:
+        friend class Shader;
+        void Setup(const GLuint &shaderProgram);
+    protected:
+        GLint _location = -1;
+        const std::string _name;
+    };
+
+    class FloatUniform;
+    typedef std::shared_ptr<FloatUniform> FloatUniformPtr;
+
+    class FloatUniform : public Uniform {
+    public:
+        static FloatUniformPtr Create(const std::string& name, const float &default_value);
+        FloatUniform(const std::string& name, const float &default_value);
+        // no copy assignment, no empty assignment
+        FloatUniform() = delete;
+        FloatUniform (const FloatUniform&) = delete;
+        FloatUniform& operator= (const FloatUniform&) = delete;
+    protected:
+        friend class infrastructure::Graphics;
+        void Set() const final;
+        void SetValue(const float &value);
+    private:
+        float _value = 0.0;
+    };
+
+    class IntUniform;
+    typedef std::shared_ptr<IntUniform> IntUniformPtr;
+
+    class IntUniform : public Uniform {
+    public:
+        static IntUniformPtr Create(const std::string& name, const int &default_value);
+        IntUniform(const std::string& name, const int &default_value);
+        // no copy assignment, no empty assignment
+        IntUniform() = delete;
+        IntUniform (const IntUniform&) = delete;
+        IntUniform& operator= (const IntUniform&) = delete;
+    protected:
+        friend class infrastructure::Graphics;
+        void Set() const final;
+        void SetValue(const int &value);
+    private:
+        int _value = 0.0;
+    };
+
+
+
+}
+
+#endif //INFRASTRUCTURE_GRAPHICS_UNIFORM_HPP
